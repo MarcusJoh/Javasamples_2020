8ffe35d337186016fdda9512d99deed2b2b82257
marcus johansson <mj.emeth@gmail.com>
2020-12-15 15:08:30 +0100

    Week 3

---
+++ Java_sample_C1_W3/Collections101/src/main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		CollectionsSample.collectionRun();
+		//HashSetsample.HashSetRun();
+
+	}
+
+
+
+}
