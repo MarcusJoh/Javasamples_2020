e753c698b4dedbdea936f347cca55c4c6d52e241
marcus johansson <mj.emeth@gmail.com>
2021-04-05 19:09:46 +0200

    Update main.java

--- Service_Java/Java_sample_C3_W3_2/13_parseCSVwithComp/src/parseCSV/main.java
+++ Service_Java/Java_sample_C3_W3_2/13_parseCSVwithComp/src/parseCSV/main.java
@@ -27,8 +27,8 @@ public class main {
 		
 		for (CSVobject row : wholeSheetObject) {
 			
-			//row.printAll();
-			// System.out.println(row.getItem());
+			row.printAll();
+			System.out.println(row.getItem());
 
 		}
 		
@@ -41,43 +41,4 @@ public class main {
 		
 		
 		
-		
-		
-//		ArrayList<String> sortHolder = new ArrayList<String>();
-//		for (int i = 0; i < wholeSheet.size(); i++) {
-//			sortHolder.add(wholeSheet.get(i).get(7));
-//
-//		}
-//
-//		Collections.sort(sortHolder);
-//		for (String row : sortHolder) {
-//			System.out.println("sorted:"+row);
-//
-//		}
-//		ArrayList<CSVobject> wholeSheetObject = new ArrayList<CSVobject>();
-//		for (int i = 0; i < wholeSheet.size(); i++) {
-//
-//			for (int y = 0; y < sortHolder.size(); y++) {
-//				if (wholeSheet.get(i).get(7).compareTo(sortHolder.get(y))) {
-//					System.out.println("comp:"+wholeSheet.get(i).get(0) + " " + sortHolder.get(y));
-//
-//					System.out.println("index:"+i + " " + y);
-//
-//					CSVobject line = new CSVobject(0, wholeSheet.get(y).get(0), wholeSheet.get(y).get(1),
-//							wholeSheet.get(y).get(2), wholeSheet.get(y).get(3), wholeSheet.get(y).get(4),
-//							wholeSheet.get(y).get(5), wholeSheet.get(y).get(6), wholeSheet.get(y).get(7));
-//					wholeSheetObject.add(line);
-//				}
-//			}
-//
-//		}
-//		System.out.println(wholeSheetObject.size());
-//		System.out.println(wholeSheetObject.get(1).getTotal());
-//		for (CSVobject row : wholeSheetObject) {
-//			// System.out.println(row.);
-//
-//		}
-
-	
 	}
-}
